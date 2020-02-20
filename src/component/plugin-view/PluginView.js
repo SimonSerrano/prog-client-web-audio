2dd92763f0c961472a32b90ce344c1b3fd1e60cb
Yon <yon@MacBook-Pro-de-Yon.local>
2020-02-20 10:47:35 +0100

    toggle route when plugin is deleted

--- src/component/plugin-view/PluginView.js
+++ src/component/plugin-view/PluginView.js
@@ -4,6 +4,7 @@ import '../../styles/buttons.css';
 import img from './image.jpg'
 import WebAudio from '../webaudio/WebAudio';
 import PluginsService from '../../utils/services/PluginsService';
+import RouteContext from '../../context/RouteContext';
 
 class PluginView extends React.Component {
     constructor(props) {
@@ -12,65 +13,78 @@ class PluginView extends React.Component {
     }
     render() {
         return (
-            <div className="wrapper">
-                <div className="header">
-                    <div className="title">{this.props.plugin.name}</div>
-                    <WebAudio></WebAudio>
-                </div>
-                <div className="card">
-                    <img alt="Plugin" src={img} height="20%" width="20%" />
-                    <div className="card_items">
-                        <div className="card_item">
-                            <div className="subtitle">
-                                Autheur :
-                                </div>
-                            <div className="inside">
-                                Marmousoft
-                                </div>
-                        </div>
-                        <div className="card_item">
-                            <div className="subtitle">
-                                Type :
-                        </div>
-                            <div className="inside">
-                                wo
-                        </div>
-                        </div>
-                        <div className="card_item">
-                            <div className="subtitle">
-                                Labels :
-                        </div>
-                            <div className="labels">
-                                <li>ceci</li>
-                                <li>est</li>
-                                <li>un</li>
-                                <li>plugin</li>
-                                <li>ouai</li>
-                                <li>gros</li>
+            <RouteContext.Consumer>{
+                ({ toggleRoute }) => {
+                    return (
+                        <div className="wrapper">
+                            <div className="header">
+                                <div className="title">{this.props.plugin.name}</div>
+                                <WebAudio></WebAudio>
                             </div>
+                            <div className="card">
+                                <img alt="Plugin" src={img} height="20%" width="20%" />
+                                <div className="card_items">
+                                    <div className="card_item">
+                                        <div className="subtitle">
+                                            Version :
+                                </div>
+                                        <div className="inside">
+                                            {this.props.plugin.version}
+                                        </div>
+                                    </div>
+                                    <div className="card_item">
+                                        <div className="subtitle">
+                                            Labels :
                         </div>
-                    </div>
-                </div>
-                <div className="description">
+                                        <div className="labels">
+                                            <li>ceci</li>
+                                            <li>est</li>
+                                            <li>un</li>
+                                            <li>plugin</li>
+                                            <li>ouai</li>
+                                            <li>gros</li>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="description">
 
-                    <div className="description_body">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                <div className="description_body">
+                                    {this.props.plugin.description}
+                                </div>
+                            </div>
+                            <div className="footer_buttons">
+                                <div className="btn" onClick={(e) => {
+                                    this._deletePlugin(this.props.plugin._id).then(
+                                        toggleRoute(undefined, undefined)
+                                    )
+                                }
+                                }>
+                                Supprimer
                         </div>
-                </div>
-                <div className="footer_buttons">
-                    <div className="btn" onClick={() => this._deletePlugin(this.props.plugin._id)}>
-                        Supprimer
-                        </div>
-                </div>
-            </div>
+
+
+                            </div>
+                        </div >
+                    )
+                }}
+            </RouteContext.Consumer>
         );
     }
-    _deletePlugin(id){
-        console.log("la");
-        const pluginsService=new PluginsService();
-        pluginsService.deletePlugin(id).then(
-            (res)=>console.log(res)
-        );
+    _deletePlugin(id) {
+        return new Promise((resolve,reject)=>{
+            const pluginsService = new PluginsService();
+            pluginsService.deletePlugin(id).then(
+                (res) => {
+                    if(res.ok){
+                        resolve()
+                    } else{
+                        reject()
+                    }
+                }
+            );
+        })
+
     }
 }
 
